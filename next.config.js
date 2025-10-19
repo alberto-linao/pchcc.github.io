052dda376f09620518110c27418a8e0aa7e8f7c4
Albert Linao <albertlinao@hotmail.com>
2025-10-19 19:13:31 +0800

    Update package to static

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export", // puts the site in /out
+  images: { unoptimized: true }, // required for static hosting
 };
 
 module.exports = nextConfig;
